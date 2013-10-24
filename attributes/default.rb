c2ec0b69784db29083a2b82fb01617705f2f1d7f
Jay Perry <jperry@brightcove.com>
2013-10-24 14:38:33 -0400

    Create attributes file and allow the source to be overridden

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['lein']['source'] = 'https://raw.github.com/technomancy/leiningen/stable/bin/lein'
+default['lein']['user'] = nil
+default['lein']['group'] = nil
+default['lein']['home'] = nil
--- recipes/default.rb
+++ recipes/default.rb
@@ -18,7 +18,7 @@
 #
 
 remote_file "/usr/local/bin/lein" do
-  source "https://raw.github.com/technomancy/leiningen/5eaad5c48db0668d773e6e964bdb64c30116c370/bin/lein"
+  source node['lein']['source']
   mode "755"
   owner "root"
   group "root"
@@ -27,8 +27,8 @@ end
 
 execute "install_leiningen" do
   command "export HTTP_CLIENT='curl --insecure -f -L -o'; lein version"
-  user   node[:lein][:user] 
-  group  node[:lein][:group]
-  environment ({"HOME" => node[:lein][:home]})
+  user   node['lein']['user']
+  group  node['lein']['group']
+  environment ({"HOME" => node['lein']['home']})
 end
 
